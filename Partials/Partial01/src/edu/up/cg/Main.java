db5fae20014a647338800493baa6464ac380a99c
bcsaalinas <betocnos2006@gmail.com>
2026-02-25 22:28:27 -0600

    add crop function and add test case

--- Partials/Partial01/src/edu/up/cg/Main.java
+++ Partials/Partial01/src/edu/up/cg/Main.java
@@ -2,6 +2,9 @@ package edu.up.cg;
 
 import edu.up.cg.io.ImageLoader;
 import edu.up.cg.io.ImageSaver;
+import edu.up.cg.model.ImageWrapper;
+import edu.up.cg.model.Region;
+import edu.up.cg.operations.ImageCrop;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -16,23 +19,15 @@ public class Main {
     BufferedImage image = loader.loadImage();
 
 
-    BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
-
-    //example of reconstructing an image read from disk
-    for (int x = 0; x < image.getWidth(); x++) {
-        for (int y = 0; y < image.getHeight(); y++) {
-
-            int color = image.getRGB(x,y);
-            newImage.setRGB(x,y,color);
 
 
+        ImageWrapper wrapper = new ImageWrapper(image);
 
-        }
-    }
+        ImageCrop crop = new ImageCrop(new Region(900,700,1300,1700));
+        wrapper = crop.apply(wrapper);
+        image = wrapper.getImage();
 
-
-    ImageSaver.saveImage(new File("test_output.jpg") , newImage, "jpg");
-
+    ImageSaver.saveImage(new File("test_output.jpg") , image, "jpg");
 
 
 
--- Partials/Partial01/src/edu/up/cg/operations/ImageCrop.java
+++ Partials/Partial01/src/edu/up/cg/operations/ImageCrop.java
@@ -1,13 +1,46 @@
 package edu.up.cg.operations;
 
 import edu.up.cg.model.ImageWrapper;
+import edu.up.cg.model.Region;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
 
 public class ImageCrop implements ImageOperation {
+
+    private final Region region;
+
+    public ImageCrop( Region region) {
+    this.region = region;
+
+    }
 
     @Override
     public ImageWrapper apply(ImageWrapper image) {
 
-    return image;
+        int x1 = region.getX1();
+        int y1 = region.getY1();
+        int x2 = region.getX2();
+        int y2 = region.getY2();
+        int width = x2 - x1;
+        int height = y2 - y1;
+
+        //create new image wrapper with the new dimensions
+        BufferedImage newImage = new BufferedImage(width, height, image.getImage().getType());
+
+
+        for (int i = x1; i < x2 ; i++) {
+
+            for (int j = y1; j < y2; j++) {
+                int rgb = image.getImage().getRGB(i,j);
+                newImage.setRGB(i-x1, j-y1, rgb);
+
+            }
+        }
+
+        //change the wrapper to the changed image
+        image.setImage(newImage);
+        return image;
     }
 
 
