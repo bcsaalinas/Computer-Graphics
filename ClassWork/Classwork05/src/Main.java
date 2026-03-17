90f064e9c67f9b0ed83191c9b87e1e126d77f282
bcsaalinas <betocnos2006@gmail.com>
2026-03-16 18:39:59 -0600

    Add initial implementation of translation API client

---
+++ ClassWork/Classwork05/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    //read arguments from command line
+    private static boolean validateArg(String[] args){
+        return false;
+    }
+
+    private static String readFile(String path){
+        return "";
+    }
+
+    private static String askLanguage(Scanner scanner){
+        return "";
+    }
+
+    //build payload to return json body as a string
+    private static String buildPayload(String fileContent, String language){
+        return "";
+    }
+
+    //hit the api with the payload and return response as a string
+    private  static String callAPI(String payload, String apiKey){
+        return "";
+    };
+
+    //recieve the api response and return the translated text
+    private static  String getTranslation(String response){
+        return"";
+    }
+
+
+    private static void writeFile (String content ,String outputPath){
+
+    }
+
+
+
+}
